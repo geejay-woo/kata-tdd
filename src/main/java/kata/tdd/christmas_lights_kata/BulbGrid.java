37b33d65983f4a1f52aa86d0eecfaf2fe8626dca
geejaywoo <geejaywoo@gmail.com>
2023-09-19 22:41:51 +0800

    #christmas_lights_kata [feat] 语义变更：业务如果有变更, 应先删除所有用例/注释掉所有用例并一个一个恢复 以保持TDD的节奏

--- src/main/java/kata/tdd/christmas_lights_kata/BulbGrid.java
+++ src/main/java/kata/tdd/christmas_lights_kata/BulbGrid.java
@@ -3,54 +3,58 @@ package kata.tdd.christmas_lights_kata;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
+/**
+ * phrase 'turn on' means: raise brightness one level
+ * phrase 'turn off' means: lower brightness one level
+ * phrase 'toggle' means: raise brightness two level
+ */
 public class BulbGrid {
 
     private int rowSize;
     private int columnSize;
-    private boolean[][] grid;
+    private int[][] grid;
 
     public BulbGrid(int columnSize, int rowSize) {
         this.rowSize = rowSize;
         this.columnSize = columnSize;
-        this.grid = new boolean[rowSize][columnSize];
+        this.grid = new int[rowSize][columnSize];
     }
 
     public void turnOnSingleBulb(int columnIndex, int rowIndex) {
-        grid[rowIndex][columnIndex] = true;
+        grid[rowIndex][columnIndex] = grid[rowIndex][columnIndex] + 1;
     }
 
     public void turnOnBulbsByRange(int startColumn, int startRow, int endColumn, int endRow) {
-        changeStatus(startColumn, startRow, endColumn, endRow, status -> true);
+        changeBrightLevelRange(startColumn, startRow, endColumn, endRow, brightLevel -> brightLevel + 1);
     }
 
     public void turnOffBulbsByRange(int startColumn, int startRow, int endColumn, int endRow) {
-        changeStatus(startColumn, startRow, endColumn, endRow, status -> false);
+        changeBrightLevelRange(startColumn, startRow, endColumn, endRow, brightLevel -> brightLevel - 1);
 
     }
 
-    public boolean isTurnOn(int columnIndex, int rowIndex) {
+    public int getBrightLevel(int columnIndex, int rowIndex) {
         return grid[rowIndex][columnIndex];
     }
 
-    public boolean isTurnOff(int columnIndex, int rowIndex) {
-        return !isTurnOn(columnIndex, rowIndex);
-    }
 
     public void toggleBulbsByRange(int startColumn, int startRow, int endColumn, int endRow) {
-        changeStatus(startColumn, startRow, endColumn, endRow, status -> !status);
+        changeBrightLevelRange(startColumn, startRow, endColumn, endRow, brightLevel -> brightLevel + 2);
     }
 
-    private void changeStatus(int startColumn, int startRow, int endColumn, int endRow, Function<Boolean, Boolean> statusGetter) {
+    private void changeBrightLevelRange(int startColumn, int startRow, int endColumn, int endRow, Function<Integer, Integer> brightLevelGetter) {
         IntStream.range(startRow, endRow + 1)
                 .forEach(row -> IntStream.range(startColumn, endColumn + 1)
-                        .forEach(column -> grid[row][column] = statusGetter.apply(grid[row][column])));
+                        .forEach(column -> {
+                            Integer brightLevelResult = brightLevelGetter.apply(grid[row][column]);
+                            grid[row][column] = brightLevelResult <= 0 ? 0 : brightLevelResult;
+                        }));
     }
 
-
-    public int getTurnOnCount() {
+    public int getTotalBrightLevel() {
         return IntStream.range(0, rowSize)
                 .flatMap(row -> IntStream.range(0, columnSize)
-                        .map(column -> grid[row][column] ? 1 : 0))
+                        .map(column -> grid[row][column]))
                 .reduce(0, Integer::sum);
     }
 }
--- src/test/java/kata/tdd/christmas_lights_kata/BulbGridTest.java
+++ src/test/java/kata/tdd/christmas_lights_kata/BulbGridTest.java
@@ -9,77 +9,86 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BulbGridTest {
 
     @Test
-    public void should_turn_on_0_0_999_999_when_turn_on() {
+    public void should_0_0_to_999_999_one_level_when_turn_on() {
         // given
 
         // when
         BulbGrid bulbGrid = new BulbGrid(1000, 1000);
-        bulbGrid.turnOnBulbsByRange(0,0,999,999);
+        bulbGrid.turnOnBulbsByRange(0, 0, 999, 999);
 
         // then
-        assertThat(bulbGrid.isTurnOn(0,0)).isTrue();
-        assertThat(bulbGrid.isTurnOn(999,0)).isTrue();
-        assertThat(bulbGrid.isTurnOn(0,999)).isTrue();
-        assertThat(bulbGrid.isTurnOn(999,999)).isTrue();
-        assertThat(bulbGrid.isTurnOn(500,500)).isTrue();
+        assertThat(bulbGrid.getBrightLevel(0, 0)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(0, 999)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(999, 0)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(999, 999)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(500, 500)).isEqualTo(1);
+        assertThat(bulbGrid.getTotalBrightLevel()).isEqualTo(1000_000);
 
     }
 
     @Test
-    public void should_0_0_turn_off_and_other_turn_on_when_toggle_given_0_0_on_and_other_off() {
+    public void should_0_0_3_level_and_other_2_level_when_toggle_given_0_0_on_and_other_off() {
         // given
 
         // when
         BulbGrid bulbGrid = new BulbGrid(1000, 1000);
-        bulbGrid.turnOnSingleBulb(0,0);
+        bulbGrid.turnOnSingleBulb(0, 0);
         bulbGrid.toggleBulbsByRange(0, 0, 999, 0);
 
         // then
-        assertThat(bulbGrid.isTurnOff(0,0)).isTrue();
-        assertThat(bulbGrid.isTurnOn(500,0)).isTrue();
-        assertThat(bulbGrid.isTurnOn(999,0)).isTrue();
+        assertThat(bulbGrid.getBrightLevel(0, 0)).isEqualTo(3);
+        assertThat(bulbGrid.getBrightLevel(500, 0)).isEqualTo(2);
+        assertThat(bulbGrid.getBrightLevel(999, 0)).isEqualTo(2);
+        assertThat(bulbGrid.getTotalBrightLevel()).isEqualTo(2000 + 1);
 
     }
 
     @Test
-    public void should_turn_off_middle_four_bulbs_when_turn_off_bulbs_given_turn_off_499_499_500_500() {
+    public void should_middle_four_bulbs_0_level_when_turn_off_bulbs_given_turn_off_499_499_500_500() {
 
         // given
 
         // when
         BulbGrid bulbGrid = new BulbGrid(1000, 1000);
-        bulbGrid.turnOnBulbsByRange(0,0,999,999);
-        bulbGrid.turnOffBulbsByRange(499,499,500,500);
+        bulbGrid.turnOnBulbsByRange(0, 0, 999, 999);
+        bulbGrid.turnOffBulbsByRange(499, 499, 500, 500);
 
         // then
-        assertThat(bulbGrid.isTurnOn(0, 0)).isTrue();
-        assertThat(bulbGrid.isTurnOn(999, 0)).isTrue();
-        assertThat(bulbGrid.isTurnOn(0, 999)).isTrue();
-        assertThat(bulbGrid.isTurnOn(999, 999)).isTrue();
-        assertThat(bulbGrid.isTurnOn(499, 499)).isFalse();
-        assertThat(bulbGrid.isTurnOn(499, 500)).isFalse();
-        assertThat(bulbGrid.isTurnOn(500, 499)).isFalse();
-        assertThat(bulbGrid.isTurnOn(500, 500)).isFalse();
+        assertThat(bulbGrid.getBrightLevel(0, 0)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(999, 0)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(0, 999)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(999, 999)).isEqualTo(1);
+        assertThat(bulbGrid.getBrightLevel(499, 499)).isEqualTo(0);
+        assertThat(bulbGrid.getBrightLevel(499, 500)).isEqualTo(0);
+        assertThat(bulbGrid.getBrightLevel(500, 499)).isEqualTo(0);
+        assertThat(bulbGrid.getBrightLevel(500, 500)).isEqualTo(0);
+        assertThat(bulbGrid.getTotalBrightLevel()).isEqualTo(1000_000 - 4);
+    }
+
+
+    @Test
+    public void should_get_0_brightness_level_when_turn_off() {
+        // given
+
+        // when
+        BulbGrid bulbGrid = new BulbGrid(1, 1);
+        bulbGrid.turnOffBulbsByRange(0, 0, 0, 0);
+
+        // then
+        assertThat(bulbGrid.getTotalBrightLevel()).isEqualTo(0);
     }
 
     @Test
-    public void should_get_right_turn_on_count() {
+    public void should_get_right_brightness_when_instruct_given_instruct() {
         // given
 
         // when
         BulbGrid bulbGrid = new BulbGrid(1000, 1000);
-        bulbGrid.turnOnBulbsByRange(887, 9, 959, 629);
-        bulbGrid.turnOnBulbsByRange(454, 398, 844, 448);
-        bulbGrid.turnOffBulbsByRange(539, 243, 559, 965);
-        bulbGrid.turnOffBulbsByRange(370, 819, 676, 868);
-        bulbGrid.turnOffBulbsByRange(145, 40, 370, 997);
-        bulbGrid.turnOffBulbsByRange(301, 3, 808, 454);
-        bulbGrid.turnOnBulbsByRange(351, 678, 951, 908);
-        bulbGrid.toggleBulbsByRange(720, 196, 897, 994);
-        bulbGrid.toggleBulbsByRange(831, 394, 904, 860);
+        bulbGrid.turnOnBulbsByRange(0, 0, 0, 0);
+        bulbGrid.toggleBulbsByRange(0, 0, 999, 999);
 
         // then
-        assertThat(bulbGrid.getTurnOnCount()).isEqualTo(230022);
+        assertThat(bulbGrid.getTotalBrightLevel()).isEqualTo(2000_000 + 1);
     }
 
 }
