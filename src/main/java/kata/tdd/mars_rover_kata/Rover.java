4e659b2d88a45844d3bad62d8eec7b0edf46137e
geejaywoo <geejaywoo@gmail.com>
2023-09-24 11:18:38 +0800

    #mars_rover_kata [refactor] 'l' instruct

--- src/main/java/kata/tdd/mars_rover_kata/Rover.java
+++ src/main/java/kata/tdd/mars_rover_kata/Rover.java
@@ -1,6 +1,7 @@
 package kata.tdd.mars_rover_kata;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Rover {
     private static final List<Character> DIRECTION_LIST = List.of('W', 'N', 'E', 'S');
@@ -17,13 +18,16 @@ public class Rover {
 
     public void execute(char instruct) {
         if (instruct == 'r') {
-            Integer newDirectionIndex = (DIRECTION_LIST.indexOf(direction) + 1) % 4;
-            this.direction = DIRECTION_LIST.get(newDirectionIndex);
+            modifyDirection(index -> index + 1);
         }
-        if(instruct == 'l') {
-            Integer newDirectionIndex = (DIRECTION_LIST.indexOf(direction) + -1) % 4;
-            this.direction = DIRECTION_LIST.get(newDirectionIndex);
+        if (instruct == 'l') {
+            modifyDirection(index -> index - 1);
         }
+    }
+
+    private void modifyDirection(Function<Integer, Integer> newDirectionIndexGetter) {
+        Integer newDirectionIndex = newDirectionIndexGetter.apply(DIRECTION_LIST.indexOf(direction)) % 4;
+        this.direction = DIRECTION_LIST.get(newDirectionIndex);
     }
 
     public int getXIndex() {
